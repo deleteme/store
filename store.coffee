3036a6c27f781fe7ea67713f73e3dfdbd3941f44
Matt Garrett <mattga@gmail.com>
2011-04-07 14:11:20 -0500

    Rewrote it so that the internal functions are created only if needed. The Store.set will safely displace data when localStorage is full. Added an uglified version.

--- store.coffee
+++ store.coffee
@@ -2,41 +2,74 @@
 # a simple wrapper around html5 local storage and cookies
 # provides three methods to interact with data
 #
+# it automatically purges data from localStorage if it's full.
+#
 # set: key, value
 #    stores the value in a key
+#    returns the value
 # get: key
 #    retrieves the value in the key
+#    returns the value
 # expire: key
 #    removes the data and the key
+#    returns the value
 
-Store: (->
-  createCookie: (name, value, days) ->
-    if days
-      date: new Date
-      date.setTime(date.getTime() + (days*24*60*60*1000))
-      expires: "; expires=" + date.toGMTString()
-    else
-      expires: ""
-    document.cookie: name + "=" + value + expires + "; path=/"
+Store = do ->
+  localStorageSupported = do ->
+    try
+      `(('localStorage' in window) && window['localStorage'] !== null)`
+    catch e
+      false
 
-  getCookie: (key) ->
-    key: key + "="
-    for cookieFragment in document.cookie.split(';')
-      while cookieFragment.charAt(0) is ' '
-        cookieFragment: cookieFragment.substring(1, cookieFragment.length) 
-      return cookieFragment.substring(key.length, cookieFragment.length) if cookieFragment.indexOf(key) == 0
-    return null
-
-  if `(('localStorage' in window) && window['localStorage'] !== null)`
+  if localStorageSupported
+    # will displace data until it can successfully save
+    safeSet = (key, value) ->
+      try
+        localStorage.setItem key, value
+        value
+      catch e
+        for num in [0..5]
+          localStorage.removeItem localStorage.key localStorage.length - 1
+        safeSet key, value
     {
-      set: (key, value) -> localStorage[key]: value
-      get: (key) -> localStorage[key]
-      expire: (key) -> localStorage.removeItem(key)
+      set: safeSet
+
+      get: (key) ->
+        localStorage[key]
+
+      expire: (key) ->
+        value = localStorage[key]
+        localStorage.removeItem(key)
+        value
     }
   else
+    createCookie = (name, value, days) ->
+      if days
+        date = new Date
+        date.setTime(date.getTime() + (days*24*60*60*1000))
+        expires = "; expires=" + date.toGMTString()
+      else
+        expires = ""
+
+      document.cookie = name + "=" + value + expires + "; path=/"
+
+      value
+
+    getCookie = (key) ->
+      key = key + "="
+      for cookieFragment in document.cookie.split(';')
+        return cookieFragment.replace(/^\s+/, '').substring(key.length, cookieFragment.length) if cookieFragment.indexOf(key) == 0
+      return null
+
     {
-      set: (key, value) -> createCookie(key, value, 1)
+      set: (key, value) ->
+        createCookie key, value, 1
+
       get: getCookie
-      expire: (key) -> createCookie(key, "", -1)
+
+      expire: (key) ->
+        value = Store.get key
+        createCookie key, "", -1
+        value
     }
-)()
+
--- store.js
+++ store.js
@@ -1,51 +1,74 @@
 var Store;
 Store = (function() {
-  var createCookie, getCookie;
-  createCookie = function(name, value, days) {
-    var date, expires;
-    if (days) {
-      date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toGMTString();
-    } else {
-      expires = "";
+  var createCookie, getCookie, localStorageSupported, safeSet;
+  localStorageSupported = (function() {
+    try {
+      return (('localStorage' in window) && window['localStorage'] !== null);
+    } catch (e) {
+      return false;
     }
-    document.cookie = name + "=" + value + expires + "; path=/";
-    return document.cookie;
-  };
-  getCookie = function(key) {
-    var _a, _b, _c, cookieFragment;
-    key = key + "=";
-    _b = document.cookie.split(';');
-    for (_a = 0, _c = _b.length; _a < _c; _a++) {
-      cookieFragment = _b[_a];
-      while (cookieFragment.charAt(0) === ' ') {
-        cookieFragment = cookieFragment.substring(1, cookieFragment.length);
+  })();
+  if (localStorageSupported) {
+    safeSet = function(key, value) {
+      var num;
+      try {
+        localStorage.setItem(key, value);
+        return value;
+      } catch (e) {
+        for (num = 0; num <= 5; num++) {
+          localStorage.removeItem(localStorage.key(localStorage.length - 1));
+        }
+        return safeSet(key, value);
       }
-      if (cookieFragment.indexOf(key) === 0) {
-        return cookieFragment.substring(key.length, cookieFragment.length);
+    };
+    return {
+      set: safeSet,
+      get: function(key) {
+        return localStorage[key];
+      },
+      expire: function(key) {
+        var value;
+        value = localStorage[key];
+        localStorage.removeItem(key);
+        return value;
       }
-    }
-    return null;
-  };
-  return (('localStorage' in window) && window['localStorage'] !== null) ? {
-    set: function(key, value) {
-      localStorage[key] = value;
-      return localStorage[key];
-    },
-    get: function(key) {
-      return localStorage[key];
-    },
-    expire: function(key) {
-      return localStorage.removeItem(key);
-    }
-  } : {
-    set: function(key, value) {
-      return createCookie(key, value, 1);
-    },
-    get: getCookie,
-    expire: function(key) {
-      return createCookie(key, "", -1);
-    }
-  };
+    };
+  } else {
+    createCookie = function(name, value, days) {
+      var date, expires;
+      if (days) {
+        date = new Date;
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+      } else {
+        expires = "";
+      }
+      document.cookie = name + "=" + value + expires + "; path=/";
+      return value;
+    };
+    getCookie = function(key) {
+      var cookieFragment, _i, _len, _ref;
+      key = key + "=";
+      _ref = document.cookie.split(';');
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        cookieFragment = _ref[_i];
+        if (cookieFragment.indexOf(key) === 0) {
+          return cookieFragment.replace(/^\s+/, '').substring(key.length, cookieFragment.length);
+        }
+      }
+      return null;
+    };
+    return {
+      set: function(key, value) {
+        return createCookie(key, value, 1);
+      },
+      get: getCookie,
+      expire: function(key) {
+        var value;
+        value = Store.get(key);
+        createCookie(key, "", -1);
+        return value;
+      }
+    };
+  }
 })();
